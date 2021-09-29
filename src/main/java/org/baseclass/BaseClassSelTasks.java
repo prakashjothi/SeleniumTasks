3819393282f49d357fc81c60ba925e9b71240942
prakashjothi <prakashjothi22@gmail.com>
2021-09-29 05:35:46 +0530

    Baseclass Screenshot Scroll

--- src/main/java/org/baseclass/BaseClassSelTasks.java
+++ src/main/java/org/baseclass/BaseClassSelTasks.java
@@ -1,7 +1,65 @@
 package org.baseclass;
 
-public class BaseClass {
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+public class BaseClassSelTasks {
 	
+	public static WebDriver driver;
+	public static TakesScreenshot ts;
+	public static JavascriptExecutor js;
+	
+	public void launchBrowser(String browserName) {
+
+		switch (browserName) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\jprne\\eclipse-workspace\\prakash\\SeleniumTasks\\src\\test\\resources\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+
+		default:
+			break;
+		}
+		
+
+	}
+	
+	public void launchURL(String url) {
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+	}
+	
+	public void screenShot() throws IOException {
+
+		ts = (TakesScreenshot)driver;
+		long time = System.currentTimeMillis();
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File des = new File("C:\\Users\\jprne\\eclipse-workspace\\prakash\\SeleniumTasks\\Screenshots\\"+time+".png");
+		FileUtils.copyFile(src, des);
+		
+
+	}
+	
+	public void scrollDown(WebElement webElement) {
+
+		js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].scrollIntoView(true)",webElement);
+
+	}
 	
 
 }
---
+++ src/test/java/org/screenshot/ScreenshotTest.java
@@ -0,0 +1,31 @@
+package org.screenshot;
+
+import java.io.IOException;
+
+import org.baseclass.BaseClassSelTasks;
+import org.testng.annotations.Test;
+
+public class ScreenshotTest extends BaseClassSelTasks {
+	
+	@Test
+	private void greenScreenshot() throws IOException {
+		
+		launchBrowser("chrome");
+		launchURL("http://www.greenstechnologys.com/");
+		
+		screenShot();
+	}
+	
+	@Test
+	private void toolsScroll() {
+
+		launchBrowser("chrome");
+		launchURL("http://toolsqa.com/");
+		
+		ToolsQA tq = new ToolsQA();
+		
+		scrollDown(tq.getLinkRecent());
+
+	}
+
+}
